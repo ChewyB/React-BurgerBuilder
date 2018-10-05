5ac3923789bbd664a75ea1213f2a9baa0db64645
ChewyB <Jb515764>
2018-10-05 10:54:27 -0700

    Burger icon has been added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,9 @@ import classes from "./Layout.module.css";
 const layout = props => (
   <>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>{props.children}</main>
+    <main className={classes.Content}>
+      {props.children}
+    </main>
   </>
 );
 
