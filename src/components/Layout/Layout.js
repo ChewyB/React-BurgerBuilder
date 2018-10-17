69d196bbf43c8dc48a120631d1c2547c80ecb394
ChewyB <Jb515764>
2018-10-16 18:24:09 -0700

    Made some progress on the sidedrawer, need to look at the video again to modify it

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import classes from "./Layout.module.css";
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 //import Aux from "../../hoc/Aux";
 
 const layout = props => (
   <>
     <Toolbar/>
+    <SideDrawer></SideDrawer>
     <main className={classes.Content}>
       {props.children}
     </main>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,17 @@
 import React from "react";
-import classes from './Toolbar.module.css'
-import Logo from '../../Logo/Logo';
+import classes from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <Logo></Logo>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+
     <nav>
-      <ul />
+      <NavigationItems />
     </nav>
   </header>
 );
