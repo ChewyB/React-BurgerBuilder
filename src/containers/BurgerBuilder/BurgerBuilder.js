d89e123ef2b7a471154969c443b1117aa265dc3e
ChewyB <j.e.beltranq@gmail.com>
2019-02-19 09:32:31 -0800

    Added test packages and some tests

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -17,7 +17,7 @@ const INGREDIENT_PRICES = {
   bacon: 0.7
 };
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   // constructor(props) {
   //   super(props);
   // }
